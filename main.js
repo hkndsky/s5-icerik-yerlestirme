ab2a27bf0a1a69d93eed74cbc3c9992b17d6e2a6
Hakan Dişkaya <hkndsky1@gmail.com>
2025-07-28 17:58:06 +0300

    Img tags has been set
    Corresponding src values has been set.

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ import { siteContent } from "./content.js";
 console.log("Site içeriği", siteContent);
 
 /* Kodlar buradan aşağıya */
+
+const navList = document.querySelectorAll("header nav a");
+
+for (let i = 0; i < navList.length; i++) {
+  navList[i].id = "nav-item-" + (i + 1);
+  navList[i].textContent = siteContent.nav[navList[i].id];
+}
+
+const imgList = document.querySelectorAll("img");
+
+imgList.forEach((el) => {
+  el.setAttribute("src", siteContent.images[el.id]);
+});
+imgList[2].setAttribute("src", siteContent.images["accent-img"]);
+/* img tagleri src değerlerini aldı.*/
